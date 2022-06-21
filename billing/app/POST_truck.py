a2f23abb5b92811b75965bce6c18dedb3c03a9ce
Rotem <kalmanrotem@gmail.com>
2022-06-21 14:50:33 +0300

    finished POST_truck.py

--- billing/app/POST_truck.py
+++ billing/app/POST_truck.py
@@ -1,24 +1,42 @@
+from http.client import OK
+import mimetypes
+from sqlite3 import IntegrityError
+from urllib import response
 import requests
 from flask import Flask, render_template, request
 import json
 from db_utils import db_utils
 
 
-mysql = db_utils()
+my_sql = db_utils()
 
 
 
 def POST_truck():
 
-    id_number = "10001"
-    provider = "something"
-    #data_insert = f"INSERT INTO Provider (name) VALUES ('{provider}');"
+    provider_id = '10003'
+    truck_id = '134-35-443'
 
-    id = mysql.getData("SELECT EXISTS(SELECT * FROM Provider WHERE id=10000);")
-    #mysql.setData(data_insert)
-    # return mysql.getData(f'select name,id from Provider where name = "{val}"')
-    return json.dumps(id)
+    id = my_sql.getData(f'SELECT EXISTS(SELECT * FROM Provider WHERE id={provider_id});')
+    x = json.dumps(id)
+    value = str(x[-3])
+
+    if value == str(0):
+        return (f"Provider id : {provider_id} not found, please try again.", 409)
     
+    else:
+
+       id_check = my_sql.getData(f'SELECT EXISTS(SELECT * FROM Trucks WHERE id="{truck_id}");')
+       y = json.dumps(id_check)
+       value_id = str(y[-3])
+
+       if value_id == str(1):
+            return (f"truck id : {truck_id} is already exist! please try again.", 409)
+
+       else:
+            my_sql.setData(f"INSERT INTO Trucks(id, provider_id) VALUES('{str(truck_id)}', '{str(provider_id)}')")
+            return (f"inserted : truck_id {truck_id} to provider id : {provider_id}", 200)
+        
 
 
 if __name__ == '__main__':
