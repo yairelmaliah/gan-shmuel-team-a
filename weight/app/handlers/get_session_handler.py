7638344873c38675d64ed4869ca3dbdf64c2940f
yairelmaliah <yairelmaliah319732@gmail.com>
2022-06-22 22:32:03 +0300

    added dummy data to transactions table

--- weight/app/handlers/get_session_handler.py
+++ weight/app/handlers/get_session_handler.py
@@ -5,13 +5,13 @@ import json
 def get_session_handler(id):
   mysql = Mysql()
   session = mysql.get_data(f"SELECT * from transactions WHERE id={id}")
-
   if not session:
     return "Invalid session ID", 404
 
   if session[0][2] == 'out':
     return json.dumps({
       "id": id,
+      "direction": session[0][2],
       "truck": session[0][3],
       "bruto": session[0][5],
       "truckTara": session[0][6],
@@ -22,7 +22,9 @@ def get_session_handler(id):
   else:
     return json.dumps({
       "id": id,
+      "direction": session[0][2],
       "truck": session[0][3],
       "bruto": session[0][5],
       "produce":session[0][8]
     })
+
--- weight/db/weightdb.sql
+++ weight/db/weightdb.sql
@@ -24,3 +24,12 @@ CREATE TABLE IF NOT EXISTS `transactions` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=10001 ;
 
+
+INSERT INTO transactions (datetime, direction, truck, containers, bruto, produce) VALUES(20220622190746, 'in', '155-34-443', 'C-3123', 5000, 'Blood');
+INSERT INTO transactions (datetime, direction, truck, containers, bruto, produce) VALUES(20220622190746, 'out','155-34-443', 'C-3123', 5000, 'Blood');
+INSERT INTO transactions (datetime, direction, truck, containers, bruto, produce) VALUES(20220622190746, 'in', '165-34-443', 'C-7123', 5000, 'Mandarin');
+INSERT INTO transactions (datetime, direction, truck, containers, bruto, produce) VALUES(20220622190746, 'in', '195-37-443', 'C-8123', 5000, 'Navel');
+INSERT INTO transactions (datetime, direction, truck, containers, bruto, produce) VALUES(20220622190746, 'out', '165-34-443', 'C-7123', 5000, 'Mandarin');
+INSERT INTO transactions (datetime, direction, truck, containers, bruto, produce) VALUES(20220622190746, 'out', '195-37-443', 'C-8123', 5000, 'Navel');
+INSERT INTO transactions (datetime, direction, truck, containers, bruto, produce) VALUES(20220622190746, 'in', '200-32-443', 'C-3123', 5000, 'Tangerine');
+INSERT INTO transactions (datetime, direction, truck, containers, bruto, produce) VALUES(20220622190746, 'out', '200-32-443', 'C-3123', 5000, 'Tangerine');
