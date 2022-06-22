6db25057abbf6d8074b779cf89e19dfd76ca609b
Oshri <oshriza@gmail.com>
2022-06-22 20:01:58 +0300

    minor changes

--- billing/app/PUT_provider.py
+++ billing/app/PUT_provider.py
@@ -5,12 +5,12 @@ from db_utils import db_utils
 
 mysql = db_utils()
 
-def data_exist():
+def data_exist(): # get data from the db and check if name exist in the 'Provider' table
     new_provider = request.args['new_provider']
     name = mysql.getData(f"SELECT * from Provider WHERE name=('{new_provider}')")
     json_str = json.dumps(name)
     resp = json.loads(json_str)
-    return resp[0]['name']
+    return resp[0]['name'] # take only the name position
 
 def update_new_provider(id):
     new_provider = request.args['new_provider']
@@ -22,10 +22,10 @@ def update_new_provider(id):
 def PUT_provider(id):
     try:
         if not data_exist():
-            return update_new_provider(id), 200 # does not exist, change to new name!
-        return f"Provider name already exist", 409 # # exist, not allowed to change to new name
+            return update_new_provider(id), 200 # provider name does not exist --> update to new name!
+        return f"Provider name already exist", 409 # # EXIST, not allowed to change to new name
     except:
-        return update_new_provider(id), 200 # does not exist, change to new name!
+        return update_new_provider(id), 200 # provider name does not exist --> update to new name!
 
 
 if __name__ == '__main__':
