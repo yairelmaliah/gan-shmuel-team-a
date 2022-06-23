6906d8d446ee421fd80ef419ae3f2c9a06500510
yairelmaliah <yairelmaliah319732@gmail.com>
2022-06-23 17:30:10 +0300

    preparing for test production

--- weight/app/test.py
+++ weight/app/test.py
@@ -1,35 +1,44 @@
 import requests
 import sys
-# 3.66.68.27
 
-PATH= "http://3.66.68.27:8083"
+url = ""
+
+if "dev" in sys.argv:
+  url = "http://localhost:5000"
+if "test" in sys.argv:
+  url = "http://3.66.68.27:8083"
+
+if not url:
+  sys.stdout.write('You must specify what environment you are in, dev||prod, eg. "python3 testing.py dev"')
+  exit(0)
+
 def test_health():
-  req = requests.get(f"{PATH}/health")
+  req = requests.get(f"{url}/health")
   status_code = req.status_code
   if (status_code < 200 or status_code > 299): return 0
   else: return 1
   
 def test_get_unknown():
-  req = requests.get(f"{PATH}/unknown")
+  req = requests.get(f"{url}/unknown")
   status_code = req.status_code
   if (status_code < 200 or status_code > 299): return 0
   else: return 1
 
 def test_get_item():
-  req = requests.get(f"{PATH}/item/1234?from=000000000000")
+  req = requests.get(f"{url}/item/1234?from=000000000000")
   status_code = req.status_code
   if (status_code < 200 or status_code > 299): return 0
   else: return 1
 
 def test_batch_weight():
-  req = requests.post(f"{PATH}/batch-weight/containers3.json")
+  req = requests.post(f"{url}/batch-weight/containers3.json")
   status_code = req.status_code
   if (status_code < 200 or status_code > 299): 
     return 0
   else: return 1
 
 def test_get_weight():
-  req = requests.get(f"{PATH}/weight")
+  req = requests.get(f"{url}/weight")
   status_code = req.status_code
   if (status_code < 200 or status_code > 299): return 0
   else: return 1
