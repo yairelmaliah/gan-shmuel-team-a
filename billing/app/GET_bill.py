c469e54943d0b04a23939d82ba8d966a84459033
Rotem <kalmanrotem@gmail.com>
2022-06-24 18:49:18 +0300

    modified get-bill - close to finish

--- billing/app/GET_bill.py
+++ billing/app/GET_bill.py
@@ -25,7 +25,7 @@ my_sql = db_utils()
    
 def GET_bill(id):
 
-     
+ ##################################### check data ####################################    
     currentDT = datetime.datetime.now()
     provider_id = str(id)
   
@@ -37,8 +37,13 @@ def GET_bill(id):
         return (f"Provider id : {provider_id} is not exist!, please try again.", 404)
 
     try:
+
         from_date = request.args['from']
         to_date = request.args['to']
+
+        if from_date > to_date:
+            return("from date is larger than to date. please try again.", 409)
+
         if len(from_date) != 14 or from_date.isnumeric() == False:
             return (f"from_date is not valid, please try again.", 409)
 
@@ -51,6 +56,7 @@ def GET_bill(id):
         to_date = currentDT.strftime("%Y%m%d%H%M%S")
 
 
+
     is_exists = my_sql.getData(f"SELECT EXISTS(SELECT id FROM Trucks WHERE provider_id='{provider_id}');")
     dump = json.dumps(is_exists)
     value = str(dump[-3])
@@ -59,8 +65,99 @@ def GET_bill(id):
         return (f"Provider id : {provider_id} does not have a license plate, please try again.", 404)
 
 
+##################################  Starting bill ##############################
+
+
     licenses = list(my_sql.getData(f'SELECT id FROM Trucks WHERE provider_id={provider_id};'))
     truck_count = len(licenses)
+    session_count = 0
+   #print(f"license : {licenses}")
+    total=0
+    
+
+
+    weighted_containers = requests.get(f"http://3.66.68.27:8081/weight?from={from_date}&to={to_date}&filter=out").json()
+    weighted_containers = json.dumps(weighted_containers)
+    weighted_containers = json.loads(weighted_containers)
+
+    
+    sessions = []
+
+  #  print(weighted_containers['data'][0])
+    #Get all sessions that are specific to this provider
+    for truck in weighted_containers['data']:
+        for truck_id in licenses:
+            if truck['truck'] == str(truck_id['id']):
+                sessions.append(truck)
+                session_count += 1
+
+
+
+    providers_products=[]
+
+    for truck in sessions:
+        flag=0
+        for product in providers_products:
+            if truck['produce'] == product['product']:
+            #    product['count'] += 1
+                product['amount'] += truck['neto']
+                product['pay'] += product['rate']*truck['neto']
+                total += product['rate']*truck['neto']
+                flag=1
+
+
+        exist_check = my_sql.getData(f"""SELECT EXISTS(SELECT product_id FROM Rates WHERE product_id='{truck['produce']}');""")
+        dump = json.dumps(exist_check)
+        value = str(dump[-3])
+        if value == str(0):  
+            return (f"Produce: {truck['produce']} does not apear in our rates, please upload new rates and try again.", 404)
+
+
+        
+        if flag == 0:
+            rate_data = my_sql.getData(f"""SELECT rate FROM Rates WHERE product_id='{truck['produce']}' AND scope={provider_id};""")
+
+
+             
+            if len(rate_data) == 0:
+                rate_data = my_sql.getData(f"""SELECT rate FROM Rates WHERE product_id='{truck['produce']}' AND scope='ALL';""")
+
+            rate_price = (rate_data[0]['rate'])
+
+
+            providers_products.append({
+            "product": truck['produce'],
+            "count" : 1,
+            "amount" : truck['neto'],
+            "rate": rate_price,
+            "pay" : truck['neto']*rate_price
+            })
+            total += rate_price*truck['neto']
+
+
+    recipet = {
+        "id": provider_id,
+        "name": "test",
+        "from": from_date,
+        "to": to_date,
+        "truckCount": truck_count,
+        "sessionCount": session_count,
+        "products": providers_products,
+        "total": total
+    }
+    
+    return recipet
+    
+if __name__ == '__main__':
+    GET_bill(id)    
+
+
+
+
+
+
+
+
 
     # truck_count = 0
     # items = []
@@ -95,70 +192,3 @@ def GET_bill(id):
     # d = requests.get(f"http://localhost:5000/weight/{license_plate}?from{from_date}to{to_date}").json()
 
   
-    name = ""
-
-    truckCount=0
-    total=0
-    
-    #We expect to get a json array for all trucks that went out
-    #headers = {"Accept": "application/json"}
-    #http://3.66.68.27:8081/weight?from=20220311203010&to=20220711203010&filter=out
-    weighted_containers = requests.get(f"http://3.66.68.27:8081/weight?from={from_date}&to={to_date}&filter=out").json()
-    weighted_containers = json.dumps(weighted_containers)
-    weighted_containers = json.loads(weighted_containers)
-    #print(type(weighted_containers['data']), flush=True)
-    #return weighted_containers
-    #TODO Number of sessions for each product
-    #TODO Get total weight of each product
-
-    providers_sessions = []
-  #  print(weighted_containers['data'][0])
-    #Get all sessions that are specific to this provider
-    for truck in weighted_containers['data']:
-        print(f"truck {truck}", flush=True)
-        for truck_id in licenses:
-            print(f"truck id : {truck_id}", flush=True)
-            print(f"truck_id 2 : {truck['truck']}", flush=True)
-            if truck['id'] == truck_id:
-                
-                print(truck['id'], flush=True)
-                providers_sessions.append(truck)
- #   print(providers_sessions, flush=True)
-    return "x"
-    print(providers_sessions,flush=True)
-    return str(providers_sessions)
-    providers_products=[]
-
-    for truck in providers_sessions:
-        flag=0
-        for product in providers_products:
-            if truck['produce'] == product['name']:
-                product['count'] += 1
-                product['amount'] += truck['neto']
-                product['pay'] += product['rate']*truck['neto']
-                total += product['rate']*truck['neto']
-                flag=1
-
-
-     #   if flag == 0:#No Products were found in the list we add it
-            #Fetch the rates for this specific provider
-           # sql_query = f"SELECT rate FROM Rates WHERE product_id={truck['produce']} AND scope={id};"
-          #  rates_arr = my_sql.getData(sql_query)
-           
-        #    if len(rates_arr) == 0:#We didn't find speific rate for this provider we have to look for ALL scope
-           #     sql_query = f"SELECT rate FROM Rates WHERE product_id={truck['produce']} AND scope='ALL'"
-                
-           #     rates_arr = my_sql.getData(sql_query)
-            #TODO for now we assume product is found in rates but that's not always the case so we have to add another check
-
-            # providers_products.append({
-            #     "name": truck['produce'],
-            #     "count" : 1,
-            #     "amount" : truck['neto'],
-            #     "rate": int(rates_arr[0][0]),
-            #     "pay" : truck['neto']*int(rates_arr[0][0])
-            # })
-            # total += int(rates_arr[0][0])*truck['neto']
-    
-if __name__ == '__main__':
-    GET_bill(id)    
