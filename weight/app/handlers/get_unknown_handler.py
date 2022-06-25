54d8153681f86b7dd71892669fddaa7510472ffe
yairelmaliah <yairelmaliah319732@gmail.com>
2022-06-26 01:11:32 +0300

    added UI

--- weight/app/handlers/get_unknown_handler.py
+++ weight/app/handlers/get_unknown_handler.py
@@ -5,6 +5,6 @@ def get_unknown_handler():
 
 	data = mysql.get_data("SELECT distinct container_id FROM containers_registered WHERE weight IS NULL")
 	if not data:
-		return "There are no containers without weight :)", 200
+		return {"data": "There are no containers without weight :)"}, 200
 	return {"data":[x[0] for x in data]}, 200
 		
